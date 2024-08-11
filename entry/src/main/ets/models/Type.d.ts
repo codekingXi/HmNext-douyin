df0aea20a7e8760c30e995a9346222db8b4c3418
张杨熙 <2362576803@qq.com>
2024-08-11 11:07:21 +0800

    删除不用的代码

--- entry/src/main/ets/models/Type.d.ts
+++ entry/src/main/ets/models/Type.d.ts
@@ -1,19 +1,7 @@
 /**
- * 商品数据类型模型*/
-export declare interface shopTypeModel {
-  "name": string
-  "cover": string
-  "imgs": string[]
-  "price": number
-  "real_price": number
-  "isLowPrice": boolean
-  "discount": string
-  "sold": number
-}
-/**
  * 一级列表可视区域的起始索引和终点索引。
  *
- * @interface
+ * @interface 索引位置接口
  * @property {number} start - 可视区域起点索引。
  * @property {number} end - 可视区域终点索引。
  */
